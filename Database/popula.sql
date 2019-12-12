6cd514183a0eb150d441507e1edb3e05228ed8b8
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2019-12-12 20:40:30 -0300

    Inicia script popula.sql (categorias e usuarios)

--- Database/popula.sql
+++ Database/popula.sql
@@ -1,2 +1,62 @@
-INSERT INTO categories (name) VALUES ('Jardinagem'), ('Elétrica'), ('Hidráulica'), ('Pintura'), ('Alvenaria'), ('Mecânica'),
+INSERT INTO categories (`name`) VALUES ('Jardinagem'), ('Elétrica'), ('Hidráulica'), ('Pintura'), ('Alvenaria'), ('Mecânica'),
 ('Transporte'), ('Buffet'), ('Babá'), ('Faxina'), ('Outros');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('187.116.990-94','Túlio Jardim', 'tuliojardim@visaojr.com.br', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9351-1055', '12.316-350');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('822.664.040-75','Tulinho Gameplays', 'tuliosjardim@gmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-5421-7584', '58.046-541');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('806.240.070-06','Clara Andrade', 'luizaandrade@hotmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9823-5123', '49.035-100');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('120.193.830-98','Eufrásio Best Jungle Br', 'elefrasio@gmail.com','$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-8757-2223', '63.122-260');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('769.909.620-70','Mateus Peres', 'mperes@bol.com.br', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-8766-2591', '68.911-055');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('657.261.950-86','Alvaro Ferreira', 'alvarofrr@gmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-7689-6535', '35.680-317');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('873.453.310-91','João Pedro Esteves', 'jpesteves@yahoo.com.br', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-8856-9012', '36.201-593');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('326.075.770-80','Sívio Luis', 'silvioluis22@ig.com.br', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9566-5231', '35.160-204');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('838.408.920-58','Alexandre Nunes', 'alexandrenunes@hotmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-8864-2323', '35.162-449');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('553.956.860-53','Antonio Rodrigues', 'antoniorod@outlook.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-8765-3245', '35.163-147');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('430.113.710-67','Rafael Castro', 'rafacastro@gmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9867-9022', '35.162-466');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('632.021.100-80','Igor Cota', 'igorcastro@gmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9854-3221', '35.162-448');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('636.156.770-21','Leonardo Araujo', 'leoaraujo@yahoo.com.br', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9657-9024', '41.204-385');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('116.310.920-73','Leandro Moreira', 'leandromoreira@gmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9677-2413', '53.565-363');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('449.193.830-02','Antonio Nunes', 'toninho@hotmail.com','$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9823-4900','88.070-030');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('473.296.980-16','Jorge Henrique', 'jorgehenrique@gmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-8865-3494', '54.280-482');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('156.815.090-36','Henrique Pedroti', 'henriquepdt@gmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9654-7786', '13.560-350');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('561.560.590-16','Luiza Cruz', 'luizacruz@hotmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9876-3466', '29.121-325');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('808.981.240-69','Ana Maria', 'anacmaria99@outlook.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9827-1231', '49.065-530');
+
+INSERT INTO users (cpf, `name`, email,`password`, phone_number, cep) VALUES
+('866.556.310-50','Marlon Castro', 'marlonc92@gmail.com', '$2y$12$D99NWJitC.uHqTUfW2hdMO39RKxYNTp2XyXSmoRnZUzeGSRuRQpxW', '(31) 9-9899-2387', '50.970-300');
